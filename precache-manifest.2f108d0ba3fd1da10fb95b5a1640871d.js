012b59f3c7dda11189dff88cc39604a61518b582
freddiewanah <freddiewanah@users.noreply.github.com>
2020-03-26 22:09:02 +0000

    Deploying to master from  - 1cd602fd582d6431c26ed494cb76206409a31321 🚀

---
+++ precache-manifest.2f108d0ba3fd1da10fb95b5a1640871d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65219336af1e408f7aba11b58750a1ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f60a3ce6b7f46bd0056",
+    "url": "/static/css/main.chunk.css"
+  },
+  {
+    "revision": "6930e24196b6ec32b1d7",
+    "url": "/static/js/4.chunk.js"
+  },
+  {
+    "revision": "94c42efb0111c04b1c84",
+    "url": "/static/js/5.chunk.js"
+  },
+  {
+    "revision": "e60e58f16e4fd1494ab4",
+    "url": "/static/js/data.chunk.js"
+  },
+  {
+    "revision": "8f60a3ce6b7f46bd0056",
+    "url": "/static/js/main.chunk.js"
+  },
+  {
+    "revision": "404c9921142257972e39ba5299652b51",
+    "url": "/static/js/main.chunk.js.LICENSE"
+  },
+  {
+    "revision": "13c9583fcd2bca6aa3ee",
+    "url": "/static/js/react.chunk.js"
+  },
+  {
+    "revision": "9300ecbebf391c6c2038edb15bc7e2cb",
+    "url": "/static/js/react.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c4e36024725dcc8ab48a",
+    "url": "/static/js/runtime-main.js"
+  },
+  {
+    "revision": "470df203f00efe828ef50b0fe86d1157",
+    "url": "/static/media/bg-12-aposles-blue.470df203.jpg"
+  },
+  {
+    "revision": "ea4c02ae50a79f6cbf54ff5d042f9563",
+    "url": "/static/media/confirmed-recent.ea4c02ae.png"
+  },
+  {
+    "revision": "b7ca8b8f4c1f410f67a2a01bc73bdd77",
+    "url": "/static/media/hospital.b7ca8b8f.png"
+  }
+]);
